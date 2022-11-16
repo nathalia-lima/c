9f53ba43eb311cf4584ef21b3bff68af7d160c05
Nathalia Lima <110511358+nathalia-lima@users.noreply.github.com>
2022-11-16 12:10:24 -0300

    Mostrar valores

---
+++ mostrar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+	
+	int a, b;
+	
+	printf("Digite o primeiro numero: ");
+	scanf("%d", &a);
+	
+	printf("Digite o segundo numero: ");
+	scanf("%d", &b);
+	
+	printf("O primeiro numero eh: %d\nO segundo numero eh: %d", a, b);
+	
+	return(0);
+}
